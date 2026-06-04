492b2b8129c616d1d87a79d00ace5ffaf375f8ff
Ultras <UltrasMiro@gmail.com>
2026-06-04 18:09:20 +0300

    Added mock for tests

--- tests/WarehouseTests.cpp
+++ tests/WarehouseTests.cpp
@@ -1,11 +1,50 @@
 #include <gtest/gtest.h>
-#include <fstream>
-#include <cstdio>
+#include <gmock/gmock.h>
+
+#include <vector>
+#include <string>
+
 #include "../models/Item.h"
 #include "../core/Warehouse.h"
-#include "../file/FileManager.h"
 
 using namespace std;
+using namespace testing;
+
+class IFileStorage {
+public:
+    virtual ~IFileStorage() = default;
+
+    virtual void saveItems(const std::vector<Item>& items) = 0;
+    virtual std::vector<Item> loadItems() = 0;
+};
+
+class MockFileStorage : public IFileStorage {
+public:
+    MOCK_METHOD(void, saveItems, (const vector<Item>& items), (override));
+    MOCK_METHOD(vector<Item>, loadItems, (), (override));
+};
+
+class WarehouseFileService {
+private:
+    IFileStorage& storage;
+
+public:
+    explicit WarehouseFileService(IFileStorage& storage) : storage(storage) {}
+
+    void saveWarehouse(const Warehouse& warehouse) {
+        storage.saveItems(warehouse.getItems());
+    }
+
+    void loadWarehouse(Warehouse& warehouse) {
+        warehouse.clear();
+
+        vector<Item> loadedItems = storage.loadItems();
+
+        for (const Item& item : loadedItems) {
+            warehouse.addItem(item);
+        }
+    }
+};
 
 TEST(WarehouseBusinessLogicTests, AddItemAndFindById_ReturnsCorrectItem) {
     Warehouse warehouse;
@@ -34,55 +73,60 @@ TEST(WarehouseBusinessLogicTests, GetTotalItems_ReturnsSumOfAllQuantities) {
     EXPECT_EQ(total, 40);
 }
 
-TEST(FileManagerTests, SaveAndLoad_ValidData_RestoresWarehouseItems) {
-    const string testFile = "test_warehouse.csv";
+TEST(FileStorageMockTests, SaveWarehouse_CallsStorageSaveWithCorrectItems) {
+    Warehouse warehouse;
 
-    Warehouse sourceWarehouse;
-    sourceWarehouse.addItem(Item(1, "Laptop", 10, "Electronics"));
-    sourceWarehouse.addItem(Item(2, "Table", 5, "Furniture"));
+    warehouse.addItem(Item(1, "Laptop", 10, "Electronics"));
+    warehouse.addItem(Item(2, "Table", 5, "Furniture"));
 
-    FileManager fileManager(testFile);
-    fileManager.save(sourceWarehouse);
+    MockFileStorage mockStorage;
+    WarehouseFileService fileService(mockStorage);
 
-    Warehouse loadedWarehouse;
-    fileManager.load(loadedWarehouse);
+    EXPECT_CALL(mockStorage, saveItems(_))
+        .Times(1)
+        .WillOnce([](const vector<Item>& items) {
+            ASSERT_EQ(items.size(), 2);
 
-    ASSERT_EQ(loadedWarehouse.getItems().size(), 2);
+            EXPECT_EQ(items[0].getId(), 1);
+            EXPECT_EQ(items[0].getName(), "Laptop");
+            EXPECT_EQ(items[0].getQuantity(), 10);
+            EXPECT_EQ(items[0].getCategory(), "Electronics");
 
-    EXPECT_EQ(loadedWarehouse.getItems()[0].getId(), 1);
-    EXPECT_EQ(loadedWarehouse.getItems()[0].getName(), "Laptop");
-    EXPECT_EQ(loadedWarehouse.getItems()[0].getQuantity(), 10);
-    EXPECT_EQ(loadedWarehouse.getItems()[0].getCategory(), "Electronics");
+            EXPECT_EQ(items[1].getId(), 2);
+            EXPECT_EQ(items[1].getName(), "Table");
+            EXPECT_EQ(items[1].getQuantity(), 5);
+            EXPECT_EQ(items[1].getCategory(), "Furniture");
+        });
 
-    EXPECT_EQ(loadedWarehouse.getItems()[1].getId(), 2);
-    EXPECT_EQ(loadedWarehouse.getItems()[1].getName(), "Table");
-    EXPECT_EQ(loadedWarehouse.getItems()[1].getQuantity(), 5);
-    EXPECT_EQ(loadedWarehouse.getItems()[1].getCategory(), "Furniture");
-
-    remove(testFile.c_str());
+    fileService.saveWarehouse(warehouse);
 }
 
-TEST(FileManagerTests, Load_CorruptedRows_SkipsInvalidRecords) {
-    const string testFile = "test_corrupted_warehouse.csv";
+TEST(FileStorageMockTests, LoadWarehouse_UsesMockedDataAndFillsWarehouse) {
+    MockFileStorage mockStorage;
+    WarehouseFileService fileService(mockStorage);
 
-    ofstream file(testFile);
-    file << "1;Laptop;10;Electronics\n";
-    file << "abc;Mouse;5;Electronics\n";
-    file << "2;Table;wrong;Furniture\n";
-    file << "3;;7;Furniture\n";
-    file << "4;Chair;12;\n";
-    file.close();
+    vector<Item> mockedItems = {
+        Item(1, "Laptop", 10, "Electronics"),
+        Item(2, "Chair", 7, "Furniture")
+    };
+
+    EXPECT_CALL(mockStorage, loadItems())
+        .Times(1)
+        .WillOnce(Return(mockedItems));
 
     Warehouse warehouse;
-    FileManager fileManager(testFile);
 
-    fileManager.load(warehouse);
+    fileService.loadWarehouse(warehouse);
 
-    ASSERT_EQ(warehouse.getItems().size(), 1);
+    ASSERT_EQ(warehouse.getItems().size(), 2);
+
     EXPECT_EQ(warehouse.getItems()[0].getId(), 1);
     EXPECT_EQ(warehouse.getItems()[0].getName(), "Laptop");
     EXPECT_EQ(warehouse.getItems()[0].getQuantity(), 10);
     EXPECT_EQ(warehouse.getItems()[0].getCategory(), "Electronics");
 
-    remove(testFile.c_str());
+    EXPECT_EQ(warehouse.getItems()[1].getId(), 2);
+    EXPECT_EQ(warehouse.getItems()[1].getName(), "Chair");
+    EXPECT_EQ(warehouse.getItems()[1].getQuantity(), 7);
+    EXPECT_EQ(warehouse.getItems()[1].getCategory(), "Furniture");
 }
